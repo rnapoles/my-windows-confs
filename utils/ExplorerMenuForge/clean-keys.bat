59c8a0cee9eaab2ea44dde80dd1b163f609e0717
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2025-07-27 00:43:45 -0500

    Remove utils/ExplorerMenuForge

--- utils/ExplorerMenuForge/clean-keys.bat
+++
@@ -1,3 +0,0 @@
-reg delete "HKEY_CLASSES_ROOT\Directory\shell\MyMenu1" /f
-reg delete "HKEY_CLASSES_ROOT\Directory\background\shell\MyMenu1" /f
-reg delete "HKEY_CLASSES_ROOT\Directory\ContextMenus\MyMenu1" /f
